a9b1977d6114e0c542cff9148b672b4c98b3ce5e
Sanchit Sharma <sanchit.sharma99@gmail.com>
2025-11-03 18:46:41 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -19,5 +19,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: '/ses-techgraph-graphite-showcase-67212/' // ⚠️ Replace with YOUR repo name
+  base: 'ses-techgraph-graphite-showcase-67212/'
 }));
